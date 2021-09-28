8c4a05e04d471374be166478d31ec995c3c0f2d9
VanshitaLavange <>
2021-09-28 09:45:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const dateOfBirth = document.querySelector('#date-of-birth');
+const luckyNumber = document.querySelector('#lucky-number');
+const checkNumberButton = document.querySelector('#check-number');
+const outputBox = document.querySelector('#output-box');
+
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+
+    outputBox.textContent = "Your birthday is lucky 😍";
+  }
+  else {
+    outputBox.textContent = "Your birthday is not lucky 😭";
+  }
+}
+
+function checkBirthDateIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateSum(dob);
+  if (dob && sum) {
+
+    compareValues(sum, luckyNumber.value);
+  }
+  else {
+    outputBox.style.color = "red";
+    outputBox.textContent = "Both the fields are mandatory 😠";
+  }
+}
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let i = 0; i < dob.length; i = i + 1) {
+    sum = sum + Number(dob.charAt(i));
+  }
+  return sum;
+}
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky)
+
